82da7ff77a13afdc7a2682f0347cb1d2bbe72b74
L.A.S.S. Kularathna <sujeewasandeepa@protonmail.ch>
2024-01-27 14:39:10 +0530

    use a dto instead of the interface

---
+++ src/certificate/dto/ResidentialCertificateData.ts
@@ -0,0 +1,134 @@
+import { IsNotEmpty, IsString, IsNumber, IsBoolean } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
+import { CivilStatus } from '../enums/CivilStatus';
+import { Religion } from '../enums/Religion';
+import { District } from '../enums/District';
+
+export class ResidentialCertificateData {
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    nic: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    fullName: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    address: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    dob: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    age: number;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    civilStatus: CivilStatus;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsBoolean()
+    isSriLankan: boolean;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    religion: Religion;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    occupation: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    dateOfResidence: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsNumber()
+    noOfElectoralRegister: number;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    nameOfFather: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    addressOfFather: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    purposeOfCertificate: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    gramaNiladhariDivName: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    gramaNiladhariDivNum: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    district: District;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    divisionalSecretariatsDiv: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    certificateType: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    sex: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    sinceWhenKnown: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsBoolean()
+    personallyKnown: boolean;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsBoolean()
+    convictedByCourtOfLaw: boolean;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsBoolean()
+    takenPartInPublicActivities: boolean;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    character: string;
+
+    @ApiProperty()
+    @IsNotEmpty()
+    @IsString()
+    remark: string;
+}
