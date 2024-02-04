c75f6fe9e5b646f0b941ed87be42a2dbb7f6941a
L.A.S.S. Kularathna <sujeewasandeepa@protonmail.ch>
2024-02-04 10:34:27 +0530

    fix error

--- src/certificate/certificate.controller.ts
+++ src/certificate/certificate.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CertificateService } from './certificate.service';
 import { ResidentialCertificateData } from './dto/ResidentialCertificateData';
+import { Response } from './types/Response';
 
 @Controller('certificate')
 export class CertificateController {
     constructor(private certificateService: CertificateService) {}
 
     @Post('residential-certificate')
-    deliveryStatus(@Body() data: ResidentialCertificateData): Promise<string> {
+    deliveryStatus(
+        @Body() data: ResidentialCertificateData,
+    ): Promise<Response> {
         return this.certificateService.residentialCertificate(data);
     }
 }
