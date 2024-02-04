679cd939dfccf5b6e95065ac918513382354c13b
L.A.S.S. Kularathna <sujeewasandeepa@protonmail.ch>
2024-02-04 10:29:22 +0530

    send a proper response

--- src/certificate/certificate.service.ts
+++ src/certificate/certificate.service.ts
@@ -1,15 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { HttpStatus, Injectable } from '@nestjs/common';
 import { generatePdf } from './utils/generatePdf';
 import { ResidentialCertificateData } from './dto/ResidentialCertificateData';
+
+type Response = {
+    status: HttpStatus;
+    message: string;
+};
 
 @Injectable()
 export class CertificateService {
     async residentialCertificate(
         data: ResidentialCertificateData,
-    ): Promise<string> {
+    ): Promise<Response> {
         const isGenerated = await generatePdf(data);
         if (isGenerated) {
-            return 'PDF generated successfully';
+            return {
+                status: HttpStatus.OK,
+                message: 'PDF generated successfully',
+            };
         } else {
             throw new Error('Failed to generate PDF');
         }
